85a925c694cff81a3cb22fedc724b6f040fff27b
Habibullo <63663696+Habibullo-1999@users.noreply.github.com>
2021-04-05 18:09:06 +0500

    Добалены функции CategoriesTotal и Тести к ниму

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -30,3 +30,13 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 
 	return sum
 }
+
+func CategoriesTotal(payments []types.Payment) map[types.Category]types.Money {
+	categories := map[types.Category]types.Money {}	
+
+	for _, payment := range payments {
+		categories[payment.Category]+=payment.Amount		
+	}
+
+	return categories
+}
